55be37c0ed665493b838b4199b495c4db061bac4
rim <rim123kaoutar@gmail.com>
2023-07-22 23:32:34 +0100

    printf can return a value

--- _printf.c
+++ _printf.c
@@ -4,19 +4,21 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	const char *str;
+	int count = 0;
 
 	va_start(args, format);
 	str = format;
 	while (*str)
 	{
-		if (*str == '%')
+		if (*str == '0')
 		{
 			str++;
 			/* (*str == '%')*/
 				
 		}
-		_putchar(*str++);
+		count += _putchar(*str);
+		str++;
 	}
 	va_end(args);
-	return (0);
+	return (count);
 }
--- main.c
+++ main.c
@@ -8,17 +8,16 @@
  */
 int main(void)
 {
-    /*int len;
+    int len;
     int len2;
-    unsigned int ui;
+    /*unsigned int ui;
     void *addr;*/
 
-	_printf("Let's try to printf a simple sentence.\n");
-     printf("Let's try to printf a simple sentence.\n");
-    /*_printf("hello!! do you see me?");
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
+    printf("len: %d\n",len);
+    printf("len: %d\n",len2);
+    /*ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
