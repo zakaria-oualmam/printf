fb14cb4faf4121560dac7467a9189c3d2a26b60b
RimKaoutar <rim123kaoutar@gmail.com>
2023-07-23 10:39:11 +0000

    add description for print_int file

--- print_int.c
+++ print_int.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_int - Prints an integer and returns its length.
+ * @n: The integer to be printed.
+ *
+ * Return: The number of characters in the integer.
+ */
+
 int print_int(int n)
 {
 	print_number(n);
